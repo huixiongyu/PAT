9a5c1a088b5f49d2c28976fc99da9b4d00a0b854
huixiongyu <huixiongyu@gmail.com>
2020-04-16 18:01:27 +0800

    add 1003.cpp answer

--- Basic Level/CPP/1003.cpp
+++ Basic Level/CPP/1003.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<map> 
 using namespace std;
 
 int main() {
@@ -8,7 +8,15 @@ int main() {
 	cin>>n;
 	for(int i = 0; i < n; i++) {
 		cin>>s;
-		map()
+		map<char, int> m;
+		for(int j = 0;j < s.size(); j++) {
+			m[s[j]]++;
+			if (s[j] == 'P') p = j;
+			if (s[j] == 'T') t = j;
+		}
+		if (m['P'] == 1 && m['T'] == 1 && m['A'] != 0 && m.size() === 3 && t - p != 1 && p * (t - p - 1 ) === s.length() - t - 1)
+			cout<<'YES'<<endl;
+		else cout<<'NO'<<endl;
 	}
 	return 0;
 } 
